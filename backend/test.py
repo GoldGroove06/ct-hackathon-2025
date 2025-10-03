fb17c5fec05da0ec9dc38c72517f3e342451b985
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-10-04 00:25:48 +0530

    polygon and new model

--- backend/test.py
+++ backend/test.py
@@ -2,13 +2,19 @@ import cv2
 import os
 import shutil
 from ultralytics import YOLO
+import numpy as np
 
-model_path = "best _new.pt"
-video_path = r"D:\projects\ct-hackathon-2025\backend\uploads\input.mp4"
-output_video_path = r"D:\projects\ct-hackathon-2025\backend\outputs\output.mp4"
-temp_dir = r"D:\projects\ct-hackathon-2025\backend\temp_frames"
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+OUTPUT_FOLDER = "outputs"
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-fps_extract = 6
+model_path = "best_new.pt"
+video_path = os.path.join(UPLOAD_FOLDER, "input_short.mp4")
+output_video_path = os.path.join(OUTPUT_FOLDER, "output.mp4")
+temp_dir = os.path.join(OUTPUT_FOLDER, "temp_frames")
+
+fps_extract = 3
 
 model = YOLO(model_path)
 
@@ -34,19 +40,32 @@ while True:
     frame_count += 1
 
 cap.release()
-print(f"✅ Extracted {saved_count} frames into {temp_dir}")
+print(f" Extracted {saved_count} frames into {temp_dir}")
 
 processed_dir = os.path.join(temp_dir, "processed")
 os.makedirs(processed_dir, exist_ok=True)
 
-for i, filename in enumerate(sorted(os.listdir(temp_dir))):
+for filename in sorted(os.listdir(temp_dir)):
     if not filename.endswith(".jpg"):
         continue
     frame_path = os.path.join(temp_dir, filename)
     results = model(frame_path)
-    results[0].save(filename=os.path.join(processed_dir, filename))
+    if results[0].masks is not None:
+    # polygons for each detection
+       for i, poly in enumerate(results[0].masks.xy):
+        polygon = np.array(poly, dtype=np.int32)  # (N,2) points
+        area = cv2.contourArea(polygon)
+        frame_yolo = results[0].plot()   # YOLO's drawn image
 
-print(f"✅ Processed frames saved into {processed_dir}")
+        cv2.putText(frame_yolo, "Custom text", (50, 50),
+            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        print(f"Polygon {i+1} → Area: {area:.2f} pixels²")
+    else:
+        print("No polygons detected")
+    
+    cv2.imwrite(os.path.join(processed_dir, filename), frame_yolo)
+
+print(f" Processed frames saved into {processed_dir}")
 
 first_frame = cv2.imread(os.path.join(processed_dir, sorted(os.listdir(processed_dir))[0]))
 height, width, _ = first_frame.shape
@@ -59,4 +78,4 @@ for filename in sorted(os.listdir(processed_dir)):
     out.write(frame)
 
 out.release()
-print(f"🎥 Output video saved as {output_video_path}")
+print(f" Output video saved as {output_video_path}")
